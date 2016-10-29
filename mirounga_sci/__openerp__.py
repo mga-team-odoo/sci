8c1404787e5e73d9c355e493e6f08b9ed833508b
Christophe CHAUVET <christophe.chauvet@gmail.com>
2016-10-29 16:31:37 +0200

    [ADD] Ajout de la structure du module

---
+++ mirounga_sci/__openerp__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 Christophe CHAUVET.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "SCI",
+    "summary": "Gestion de Société Civile Immobilière",
+    "version": "8.0.1.0.0",
+    'category': 'Localization',
+    "website": "http://mirounga.fr/",
+    "author": "Christophe CHAUVET, Mirounga",
+    "license": "AGPL-3",
+    "application": True,
+    "installable": True,
+    # "pre_init_hook": "pre_init_hook",
+    # "post_init_hook": "post_init_hook",
+    # "post_load": "post_load",
+    # "uninstall_hook": "uninstall_hook",
+    "external_dependencies": {
+        "python": [],
+        "bin": [],
+    },
+    "depends": [
+        "base",
+        "account",
+    ],
+    "data": [],
+    "demo": [],
+    "qweb": []
+}
